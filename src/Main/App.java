6adb905e57d909956688deab058c094369eb15a4
ryuzakikami <youcefrezgwi@gmail.com>
2025-04-05 11:42:25 +0200

    quicksort

--- src/Main/App.java
+++ src/Main/App.java
@@ -1,17 +1,16 @@
 package Main;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.*;
-import Main.Sort;
+import java.util.Random;
+import java.util.random.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int z [] = {33,23,3,111,21};
-        Sort s = new Sort();
-        int[] y  = s.selectionsortMax(z);
-        int[] c  = s.selectionsortMin(z);
-        s.printarray(y);
-        System.out.println("");
-        s.printarray(c);
+      Random rand = new Random();
+        int[] x = {3434,3465345,2,5};
+
+        int[] z = Sort.quicksort(x, 0, x.length-1);
+
+        Sort.printarray(z);
+
+        
     }
 }
--- src/Main/Sort.java
+++ src/Main/Sort.java
@@ -1,21 +1,20 @@
 package Main;
-import java.util.Arrays;
+import java.util.random.*;
+
 
 public class Sort{
 
 public int [] selectionsortMax(int arr[]){
   int x[] = arr.clone(); 
   int max = 0;
-
+   
   for (int i = 0; i < x.length; i++) {
     max = i;
     for (int j = i+1; j < x.length; j++) {
         max = x[max]< x[j] ? j : max;
     }
     if(max !=i){
-     int temp = x[max];
-     x[max] = x[i];
-     x[i] = temp;
+      swap(arr, max, i);
     }
   }
   return x;
@@ -31,21 +30,43 @@ public int [] selectionsortMin(int arr[]){
         min = x[j]< x[min] ? j : min;
       }
       if(min !=i){
-       int temp = x[min];
-       x[min] = x[i];
-       x[i] = temp;
+      swap(arr, min, i);
       }
     }
     return x;
   }
- public int [] quicksort(int ary[]){
-     
 
+  
+ public static int[] quicksort(int[] array,int lowIndex, int highindex){
+      int pivot = array[highindex];
+      int leftpointer = lowIndex;
+      int rightpointer = highindex;
+     if(lowIndex >=highindex){
+      return null;
+     } 
+      while (leftpointer<rightpointer) {
+        while (array[leftpointer]<= pivot && leftpointer <rightpointer) {
+          leftpointer++;
+        }
+        while (array[rightpointer]>= pivot && leftpointer <rightpointer) {
+          rightpointer--;
+        }
+        swap(array, leftpointer, rightpointer);
+      }
     
-    return ary;
+       swap(array, leftpointer,highindex);
+        
+       quicksort(array, lowIndex, leftpointer-1);
+       quicksort(array, leftpointer +1, highindex);
+    return array;
  } 
 
-public void printarray(int[]arr){
+ public static void swap(int array[],int index1, int index2){
+  int temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+ }
+public static void printarray(int[]arr){
 for (int i = 0; i < arr.length; i++) {
     System.out.print("[");
     System.out.print(arr[i]);
