e0037cc6803248c1095d744f681cdf3690593d3e
ryuzakikami <youcefrezgwi@gmail.com>
2025-04-06 20:02:26 +0200

    Tail hinzugefüht

--- src/Main/Linkedlist.java
+++ src/Main/Linkedlist.java
@@ -1,11 +1,13 @@
 package Main;
 import java.util.LinkedList;
 public class Linkedlist {
-private Node Head ;
+private Node Head;
+private Node tail;
 
 
-public Linkedlist(Node Head){
+public Linkedlist(Node Head, Node tail){
  this.Head = Head;   
+ this.tail = tail;
 }
 
 public void add(Node nodebeforNode, int nodetoadd){
@@ -17,7 +19,11 @@ public void newHead(int nodetoadd){
    Node newHead = new Node(nodetoadd,this.Head);
    this.Head = newHead;
   }
-
+public void newTail(int nodetoadd){
+    Node newTail = new Node(nodetoadd,null);
+    this.tail.setNextNode(newTail);
+    this.tail = newTail;
+}
 
 public void pop(Node nodetodelete){
  if(nodetodelete == null || nodetodelete.getNextNode() == null){
@@ -41,6 +47,13 @@ System.out.println("Null");
 
 public Node getHead() {
     return Head;
+}
+
+public Node getTail() {
+    return tail;
+}
+public void setTail(Node tail) {
+    this.tail = tail;
 }
     
 }
